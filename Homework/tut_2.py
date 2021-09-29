958629f5a0e372d3eee879d949f51ef4b0dc2a40
Bekhzod Odilov <behzododilov97@gmail.com>
2021-09-29 17:32:28 +0500

    tutorial 2 homwork update first

---
+++ Homework/tut_2.py
@@ -0,0 +1,58 @@
+print(10-7) 
+
+
+
+a = "Bekhzod"
+b = "Odilov"
+
+print(a+" "+b)
+
+
+
+
+variable = '''
+BU
+Mening 
+2-darsim
+'''
+print(variable)
+
+
+
+brinchi_qator="BU"
+brinchi_qator+=" Mening"
+brinchi_qator+=''' 2-
+darsim'''
+print(brinchi_qator)
+
+
+
+x=2
+x+=5
+x**=3
+x//=2
+x-=1
+print(x)
+
+
+
+print("*********\n*\n*\n*********\n\t*\n\t*\n*********")
+
+
+
+n=shimol
+s=Janub
+w=Garb
+e=Sharq
+
+print("        Shimol\n\nG'arb\t\t Sharq \n\n\n\tJanub")
+
+
+name=input("Ismingizni yozing  ")
+surname=input("Familyangiz  ")
+print(f"Salom, {name} {surname}, Siz bilan tanishganimdan xursandman!")
+
+
+print("{}ning {}isimli dugonasi va {} ismli singlisi bor" .format ("Muxlisa", "Munira", "Muyassar"))
+print("{2}ning {1}isimli dugonasi va {0} ismli singlisi bor".format ("Muxlisa", "Munira", "Muyassar")) 
+
